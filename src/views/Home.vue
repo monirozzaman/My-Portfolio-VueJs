e2eedeadd56f985866267ae8f47c5cdac8ca94f2
Monirozzaman Rony <monirozzaman.rony@ibcs-primax.com>
2020-12-09 12:23:12 +0600

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<Header />
+<Body />
+<Footer />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from '../components/Header/Header.vue'
+import Body from '../components/Body/Body.vue'
+import Footer from '../components/Footer/Footer.vue'
+
 
 export default {
-  name: "Home",
   components: {
-    HelloWorld
+    Header,
+    Body,
+    Footer,
   }
-};
+}
 </script>
